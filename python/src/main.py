a8cd782acb22b342026c404ba5b51e66aa294e6f
Buttery <32206665+ButteryJee@users.noreply.github.com>
2024-03-05 20:34:44 -0600

    Catch exception if the arduino is not connected. (#1)

--- python/src/main.py
+++ python/src/main.py
@@ -1,11 +1,22 @@
 from arduino import Arduino
 import serial
+import logging
+import sys
 
 COM_PORT = "COM3"
 BAUD_RATE = 9600
-ARDUINO = serial.Serial(port=COM_PORT, baudrate=BAUD_RATE, timeout=.1)
 
 if __name__ == "__main__":
-    proc = Arduino(ARDUINO)
+
+    # Attemp to connect to arduino device
+    try:
+        connection = serial.Serial(port=COM_PORT, baudrate=BAUD_RATE, timeout=.1)
+    except serial.SerialException:
+        logging.error("Could not connect to device on {} with baud {}.".format(COM_PORT, BAUD_RATE))
+        sys.exit(1)
+        
+
+    # Continue if connection was successful
+    proc = Arduino(connection)
     for i in range(10):
         print(proc.cmd_get_temp_humid())
